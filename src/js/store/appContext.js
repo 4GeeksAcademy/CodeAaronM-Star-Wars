03b28412a50af1c2844e6bfd243dc70199ea29b8
Hernán García <hernanjkd@gmail.com>
2019-10-25 05:36:02 +0000

    preparing for pull request

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -18,14 +18,9 @@ const injectContext = PassedComponent => {
 		);
 
 		useEffect(() => {
-			setState({
-				store: {
-					...state.store,
-					temp:
-						"This key will be overwritten because state.store does not reflect anything that is not hardcoded."
-				},
-				actions: state.actions
-			});
+			let text =
+				"This key will be overwritten because state.store does not reflect anything that is not hardcoded.";
+			setState({ store: { ...state.store, temp: text }, actions: state.actions });
 			setState({ store: { ...state.store, contacts: [1, 2, 3, 4, 5] }, actions: state.actions });
 			setTimeout(() => console.log("APPCONTEXT", state.store), 2000);
 		}, []);
