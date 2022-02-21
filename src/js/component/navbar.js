9f8c482829f7e189e7cc420c7ba1e07e24e84ba7
Jose <josejesuscedeno@gmail.com>
2022-02-21 20:35:30 +0000

    trabajando en el navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import trash from "../../img/trash.png"
+import logo from "../../img/logo.png"
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1 mx-5"><img src={logo} /></span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+				<div class="dropdown mx-5">
+  <a class="btn btn-primary dropdown-toggle " href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false" >
+    Favorites
+  </a>
+
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <li><a class="dropdown-item" href="#">Luke Skywalker</a><img src={trash} /></li>
+    <li><a class="dropdown-item" href="#">Darth Vader</a></li>
+    <li><a class="dropdown-item" href="#">Princess Leia</a></li>
+  </ul>
+</div>
 				</Link>
 			</div>
 		</nav>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,20 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import trash from "../../img/trash.png"
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+		<h1>Hola caraechola</h1>
 		<p>
 			<img src={rigoImage} />
 		</p>
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
 		</a>
+		<p>
+			<img src={trash} />
+		</p>
+	
 	</div>
 );
