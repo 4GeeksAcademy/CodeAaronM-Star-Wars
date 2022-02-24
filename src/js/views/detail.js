61e86749a7bd97ad494726527487c3ed7da52de4
Jose <josejesuscedeno@gmail.com>
2022-02-24 16:44:00 +0000

    listo para subir detail

--- src/js/views/detail.js
+++ src/js/views/detail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import luke from "../../img/luke.jpg"
+import "../../styles/index.css";
 
 export const Detail = () => {
     return(
@@ -20,6 +21,26 @@ export const Detail = () => {
             
           </div>
           <div><hr className="redBar"/></div>
+          <div class="d-flex bd-highlight red">
+  <div class="p-2 flex-fill bd-highlight">
+    <p><strong>Name</strong></p>
+    <p>Luke Skywalker</p></div>
+  <div class="p-2 flex-fill bd-highlight">
+    <p><strong>Birth Year</strong></p>
+    <p>19BBY</p></div>
+  <div class="p-2 flex-fill bd-highlight">
+    <p><strong>Gender</strong></p>
+    <p>male</p></div>
+  <div class="p-2 flex-fill bd-highlight">
+    <p><strong>Height</strong></p>
+    <p>172</p></div>
+  <div class="p-2 flex-fill bd-highlight">
+    <p><strong>Skin Color</strong></p>
+  <p>fair</p></div>
+  <div class="p-2 flex-fill bd-highlight">
+    <p><strong>Eye Color</strong></p>
+  <p>blue</p></div>
+</div>
         </div>
       </div>
     );
