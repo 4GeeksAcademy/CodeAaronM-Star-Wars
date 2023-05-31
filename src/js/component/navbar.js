d271f3c37633b4f201a74aad1be4797c6b66efef
Alefantozzi86 <119671613+Alefantozzi86@users.noreply.github.com>
2023-05-31 22:13:31 +0000

    footer y navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,11 +5,11 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<img src= "https://i.pinimg.com/originals/9e/4b/8c/9e4b8cd05a0e5c0b1e9237d9013417af.png" className="navbar-brand mb-0 h1 m-3" style={{width: 6 + 'rem'}}/>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button  className="btn btn-primary m-4">Favoritos</button>
 				</Link>
 			</div>
 		</nav>
