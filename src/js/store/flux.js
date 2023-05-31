3670c0ea6e783189cc43864e19c7fa25053a66e5
Alefantozzi86 <119671613+Alefantozzi86@users.noreply.github.com>
2023-05-31 22:25:41 +0000

    appContext

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,6 +1,11 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			characters:[],
+			planets:[],
+			images:[],
+			favorites:[],
+
 			demo: [
 				{
 					title: "FIRST",
