e7be0329342ec43a538f152c09ae41b4ebd17fe6
Osián <149536063+OsianJL@users.noreply.github.com>
2024-02-27 20:35:25 +0000

    first step

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,48 @@
 const getState = ({ getStore, getActions, setStore }) => {
+
+	
 	return {
+		
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			people: [],
+			planets: [],
+			starships: [],
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			getPeople: () => {
+				
+				fetch("https://www.swapi.tech/api/people/")
+				.then(res => res.json())
+				.then(data => setStore({people: data.results}))
+				.catch(err => console.error(err))
+				
+			},
+			getPlanets: () => {
+				
+				fetch("https://www.swapi.tech/api/planets/")
+				.then(res => res.json())
+				.then(data => setStore({planets: data.results}))
+				.catch(err => console.error(err))
+				
+			},
+			getStarships: () => {
+				
+				fetch("https://www.swapi.tech/api/starships/")
+				.then(res => res.json())
+				.then(data => setStore({starships: data.results}))
+				.catch(err => console.error(err))
+				
 			}
+		
+		
+		
+		
+		
+		
+		}
 		}
 	};
-};
+	
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,80 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext, useEffect } from "react";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+  
+	useEffect(() => {
+	  actions.getPeople();
+	 }, []);
+	console.log(store.people);
+	useEffect(() => {
+	  actions.getPlanets();
+	  
+	}, []);
+	console.log(store.planets);
+  
+	useEffect(() => {
+	  actions.getStarships();
+	  
+	}, []);
+	console.log(store.starships);
+  
+	return (
+	  <div className="container overflow-scroll">
+		<div className="card-people">
+		  {store.people.map((item, index) => (
+			<div className="card-group" key={index}>
+			  <div className="card">
+				<img src="..." className="card-img-top" alt="..." />
+				<div className="card-body">
+				  <h5 className="card-title">{item.name}</h5>
+				  <p className="card-text">handsome</p>
+				  <p className="card-text">
+					<small className="text-muted">blond electric hair</small>
+				  </p>
+				</div>
+			  </div>
+			</div>
+		  ))}
+		</div>
+  
+		<div className="card-planets">
+		  {store.planets.map((item, index) => (
+			<div className="card-group" key={index}>
+			  <div className="card">
+				<img src="..." className="card-img-top" alt="..." />
+				<div className="card-body">
+				  <h5 className="card-title">{item.name}</h5>
+				  <p className="card-text">handsome</p>
+				  <p className="card-text">
+					<small className="text-muted">blond electric hair</small>
+				  </p>
+				</div>
+			  </div>
+			</div>
+		  ))}
+		</div>
+  
+		<div className="card-starships">
+		  {store.starships.map((item, index) => (
+			<div className="card-group" key={index}>
+			  <div className="card">
+				<img src="..." className="card-img-top" alt="..." />
+				<div className="card-body">
+				  <h5 className="card-title">{item.name}</h5>
+				  <p className="card-text">handsome</p>
+				  <p className="card-text">
+					<small className="text-muted">blond electric hair</small>
+				  </p>
+				</div>
+			  </div>
+			</div>
+		  ))}
+		</div>
+	  </div>
+	);
+  };
+  
