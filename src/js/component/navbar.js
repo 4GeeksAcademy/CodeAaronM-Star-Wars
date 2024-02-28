1cff1360490912df1a75dccffac7270b63807704
Osián <149536063+OsianJL@users.noreply.github.com>
2024-02-28 10:30:14 +0000

    layout first steps

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,11 +5,11 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1 ms-5"><img style={{width:"80px", height:"50px"}} src="https://cdn.imgbin.com/19/21/17/imgbin-skellig-michael-chewbacca-star-wars-computer-icons-star-wars-1K3isTPsdLiNwqzBDYgVAcqvU.jpg"></img></span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-primary me-5">Favourites</button>
 				</Link>
 			</div>
 		</nav>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,45 +3,74 @@ import "../../styles/home.css";
 import { Context } from "../store/appContext";
 
 
+
 export const Home = () => {
 	const { store, actions } = useContext(Context);
-  
+
 	useEffect(() => {
-	  actions.getPeople();
-	 }, []);
+		actions.getPeople();
+	}, []);
 	console.log(store.people);
 	useEffect(() => {
-	  actions.getPlanets();
-	  
+		actions.getPlanets();
+
 	}, []);
 	console.log(store.planets);
-  
+
 	useEffect(() => {
-	  actions.getStarships();
-	  
+		actions.getStarships();
+
 	}, []);
 	console.log(store.starships);
-  
+
 	return (
-	  <div className="container overflow-scroll">
-		<div className="card-people">
-		  {store.people.map((item, index) => (
-			<div className="card-group" key={index}>
-			  <div className="card">
-				<img src="..." className="card-img-top" alt="..." />
-				<div className="card-body">
-				  <h5 className="card-title">{item.name}</h5>
-				  <p className="card-text">handsome</p>
-				  <p className="card-text">
-					<small className="text-muted">blond electric hair</small>
-				  </p>
-				</div>
-			  </div>
+		<div className="base container">
+			<h2 className="text-danger">Characters</h2>
+			<div className="card-people d-flex">
+				{store.people.map((item, index) => (
+					<div className="card-group" key={index}>
+						<div className="card">
+							<img src="..." className="card-img-top" alt="..." />
+							<div className="card-body">
+								<h5 className="card-title">{item.name}</h5>
+								<p className="card-text">handsome</p>
+								<p className="card-text">handsome</p>
+								<p className="card-text">handsome</p>
+								
+							</div>
+							<div className="footer">
+							<button className="boton-learn">Learn More!</button>
+							<button className="boton-heart"><i class="fa-regular fa-heart"></i></button>
+							
+							</div>
+						</div>
+					</div>
+				))}
 			</div>
-		  ))}
-		</div>
-  
-		<div className="card-planets">
+			<h2 className="text-danger">Characters</h2>
+			<div className="card-people d-flex overflow-x">
+				{store.people.map((item, index) => (
+					<div className="card-group" key={index}>
+						<div className="card">
+							<img src="..." className="card-img-top" alt="..." />
+							<div className="card-body">
+								<h5 className="card-title">{item.name}</h5>
+								<p className="card-text">handsome</p>
+								<p className="card-text">handsome</p>
+								<p className="card-text">handsome</p>
+								
+							</div>
+							<div className="footer">
+							<button className="boton-learn">Learn More!</button>
+							<button className="boton-heart"><i class="fa-regular fa-heart"></i></button>
+							
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+
+			{/* <div className="card-planets">
 		  {store.planets.map((item, index) => (
 			<div className="card-group" key={index}>
 			  <div className="card">
@@ -73,8 +102,7 @@ export const Home = () => {
 			  </div>
 			</div>
 		  ))}
+		</div> */}
 		</div>
-	  </div>
 	);
-  };
-  
+};
