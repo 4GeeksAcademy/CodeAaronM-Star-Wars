12e3f8d015f70f4706602a2bae4b96c456dbb0ff
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-03-06 02:27:55 +0000

    :bug: TENEMOS UN BUG :C

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,22 +12,19 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			home: [],
+			search: ""
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
 			changeColor: (index, color) => {
 				//get the store
 				const store = getStore();
-
+				console.log(store)
 				//we have to loop the entire demo array to look for the respective index
 				//and change its color
 				const demo = store.demo.map((elm, i) => {
@@ -37,6 +34,20 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
+			},
+			getAllAgendas: async () => {
+				try{
+					const res = await fetch("https://playground.4geeks.com/apis/fake/contact/agenda")
+					const data = await res.json()
+					setStore({home: data})
+				}catch(error){}
+			},
+			toggleChange: ({target})=>{
+				setStore({search: target.value})
+				const store = getStore();
+				const agendas = store.home
+				const filteredAgendas = agendas.filter(agenda => agenda.includes(target.value))
+				setStore({home: filteredAgendas})
 			}
 		}
 	};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,7 +1,36 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useContext, useEffect, useState } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+
 import "../../styles/home.css";
 
-export const Home = () => (
-	<h1>Contact List</h1>
-);
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	useEffect(()=>{
+		actions.getAllAgendas()
+	},[])
+	
+	const agendaMap = store.home.map((user,index) => <div key={index}>
+		<Link>{user}</Link>
+	</div>)
+
+	// const toggleChange = ({target})=>{
+	// 	setSearch(target.value)
+	// 	const agendas = store.home
+	// 	const filteredAgendas = agendas.filter(agenda => agenda.includes(target.value))
+	// 	console.log(filteredAgendas)
+	// }
+
+	return(
+		<div className="text-center">
+			<h1>List of Agendas</h1>
+			<input type="text" placeholder="Write Your Name Here" value={store.search} onChange={(event)=>actions.toggleChange(event)}></input>
+			<div>
+				{agendaMap}
+			</div>
+		</div>
+	)
+}
+
