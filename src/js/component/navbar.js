f5b655e72ab0ae158f2fac29ae8d55e52c63aa4e
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-03-08 00:20:17 +0000

    :lipstick: css from register finished and adding some styling to navbar and footer

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../img/logo.png"
+import "../../styles/global.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<Link to="/">
-				<span className="navbar-brand mb-0 ms-2">GossipAgenda</span>
+		<nav className="navbar navbar-light bg-light shadow-b">
+			<Link to="/" style={{width: "250px"}}>
+				<img className="logo" src={logo}></img>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/register">
-					<button className="btn btn-primary me-2">Register a New Agenda</button>
+					<button className="btn btn-danger me-2 btn-active-none">Register a New Agenda</button>
 				</Link>
 			</div>
 		</nav>
--- src/js/views/register.js
+++ src/js/views/register.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 import "../../styles/register.css";
+import logo from "../../img/logo.png"
 
 export const Register = () => {
 	const { store, actions } = useContext(Context);
@@ -11,12 +12,20 @@ export const Register = () => {
 	return (
 		<div className="register-wrapper">
 			<div className="d-flex justify-content-center align-items-center">
-				<div className="d-flex flex-column bg-white p-3 border rounded-3 mx-2" style={{minWidth:"60%"}} >
-					<h2>Just a few steps away for your agenda!</h2>
-					<label htmlFor="userName">Insert Agenda's Name</label>
-					<input className="form-control" type="text" name="userName"></input>
-					<label htmlFor="favColor">What is your favorite color</label>
-					<input className="form-control" type="text" name="favColor"></input>
+				<div className="d-flex flex-column bg-white rounded-2 p-3 mx-2 gap-2" style={{width:"50%"}} >
+					<img className="logo" src={logo}></img>
+					<Link to="/"className="btn btn-danger">Back To Home</Link>
+					<div className="landr">
+						<div className="left"></div>
+						<div classname="circle" style={{margin: "5px", color:"#b1b6bd"}}>o</div>
+						<div classname="right" style={{borderTop: "1px solid #e2e8f0", flexGrow: "1"}}></div>
+					</div>
+					<h2 className="mb-2">Register a New Agenda!</h2>
+					<label className="" htmlFor="userName">Insert Agenda's Name</label>
+					<input className="form-control" type="text" name="userName" placeholder="Example Name"></input>
+					<label className="" htmlFor="favColor">What is your favorite color</label>
+					<input className="form-control" type="text" name="favColor" placeholder="Red"></input>
+					<button className="btn btn-danger mt-3">Register Agenda</button>
 				</div>
 			</div>
 			<div className="bg-img">
