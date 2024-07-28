1385fc1dd48a642c67969e55eecf779f0e18d13d
Jose Antonio Tovar <136499634+Joseblue11@users.noreply.github.com>
2024-07-28 07:39:15 +0000

    prueba 2

--- src/js/component/contact.js
+++ src/js/component/contact.js
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 
 export const Contact = () => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setShowSuccessMessage(true); // Show success message after form submission
-  };
-
   return (
     <div className="pt-5" id="Contactos">
-      <form onSubmit={handleSubmit} action="https://formsubmit.co/josea.tovarp.blue7@gmail.com" method="POST" className="pt-4">
+      <form
+        action="https://formsubmit.co/josea.tovarp.blue7@gmail.com"
+        method="POST"
+        className="pt-4"
+      >
         <div className="d-flex justify-content-center">
-          <div className="card1 mb-3 justify-content-center" style={{ width: "30rem", height: "30rem" }}>
+          <div
+            className="card1 mb-3 justify-content-center"
+            style={{ width: "30rem", height: "30rem" }}
+          >
             <div className="card-header mb-3">
-              <h3>Contáctanos</h3>
+              <h3> Contáctanos</h3>
             </div>
             <div className="card-body">
               <div className="form-floating mb-3">
@@ -23,8 +23,8 @@ export const Contact = () => {
                   name="name"
                   className="form-control"
                   id="floatingInput"
-                  placeholder="Nombre y Apellido"
-                  style={{ width: "100%" }}
+                  placeholder="name-lastname"
+                  style={{ width: "25rem" }}
                 />
                 <label htmlFor="floatingInput" style={{ color: "gray" }}>
                   Nombre y Apellido
@@ -37,7 +37,7 @@ export const Contact = () => {
                   className="form-control"
                   id="floatingInput"
                   placeholder="phone"
-                  style={{ width: "100%" }}
+                  style={{ width: "25rem" }}
                 />
                 <label htmlFor="floatingInput" style={{ color: "gray" }}>
                   Telefono
@@ -50,35 +50,44 @@ export const Contact = () => {
                   className="form-control"
                   id="floatingInput"
                   placeholder="email"
-                  style={{ width: "100%" }}
+                  style={{ width: "25rem" }}
                 />
                 <label htmlFor="floatingInput" style={{ color: "gray" }}>
                   Correo electronico
                 </label>
               </div>
               <div className="form-floating mb-3">
-                <textarea
+                <input
                   type="message"
                   name="message"
                   className="form-control"
                   id="floatingInput"
                   placeholder="message"
-                  style={{ width: "100%", height: "10rem" }}
+                  style={{ width: "25rem", height: "10rem" }}
                 />
                 <label htmlFor="floatingInput" style={{ color: "gray" }}>
                   Mensaje
                 </label>
               </div>
-              <input type="submit" value="Enviar" className="btn btn-primary" id="submitButton" />
-              {showSuccessMessage && ( // Conditionally render success message
-                <div id="successMessage">
-                  <p>
-                    Agradecemos su interés en comunicarse con nosotros. Su consulta será atendida a la brevedad, en un plazo máximo de 24 horas.
-                  </p>
-                </div>
-              )}
-              <input type="hidden" name="_next" value="https://financity-vert.vercel.app/" />
-              <input type="hidden" name="_captcha" value="false" />
+              <input
+                class="btn btn-primary"
+                type="submit"
+                value="enviar"
+                id="submitButton"
+              ></input>
+              <div id="successMessage" style="display: none;">
+                <p>
+                  Agradecemos su interés en comunicarse con nosotros. Su
+                  consulta será atendida a la brevedad, en un plazo máximo de 24
+                  horas.
+                </p>
+              </div>
+              <input
+                type="hidden"
+                name="_next"
+                value="https://financity-vert.vercel.app/"
+              ></input>
+              <input type="hidden" name="_captcha" value="false"></input>
             </div>
           </div>
         </div>
